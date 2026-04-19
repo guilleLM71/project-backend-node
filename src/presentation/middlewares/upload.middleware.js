e11fe8f241d8089fced18ff16dceabcb44ea669f
guilleLM71 <jlauram3@fcpn.edu.bo>
2026-04-18 23:11:41 -0400

    ajuste de subida src

--- src/presentation/middlewares/upload.middleware.js
+++ src/presentation/middlewares/upload.middleware.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 import path from 'path';
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/'), 
+    destination: (req, file, cb) => cb(null, 'src/uploads/'), 
     filename: (req, file, cb) => {
         const suffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         cb(null, suffix + path.extname(file.originalname));
